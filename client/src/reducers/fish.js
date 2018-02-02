ba6d788b9cd6a58049c342df94aea5798556c237
Cameron Earl <developer.cameron@gmail.com>
2018-02-02 15:32:59 -0700

    Chained array methods in fish reducer @anthonykulis

--- client/src/reducers/fish.js
+++ client/src/reducers/fish.js
@@ -24,28 +24,23 @@ export default (state = initialState, action) => {
 			}
 		case GET_FISH_SUCCESS:
 			const getWeight = ({ length, girth }) => length * girth * girth / 800
-			const weightedFishArr = action.fishArr.map(f => ({
-				...f,
-				weight: getWeight(f),
-			}))
-			const sortedFishArr = weightedFishArr.sort((a, b) => b.weight - a.weight)
-			const finalFishArr = sortedFishArr.map(f => ({
-				...f,
-				weight: Math.floor(f.weight + 0.5), //rounding
-			}))
 
-			let newFish = { flathead: [], blue: [], channel: [] }
-			//could use 3 filter methods, but one for loop is much faster
-			for (let fish of finalFishArr) {
-				newFish[fish.species].push(fish)
-			}
-
+			let nextState = action.fishArr
+				.map(f => ({ ...f, weight: getWeight(f) }))
+				.sort((a, b) => b.weight - a.weight)
+				.map(f => ({ ...f, weight: Math.round(f.weight) }))
+				.reduce(
+					(obj, f) => {
+						obj[f.species].push(f)
+						return obj
+					},
+					{ flathead: [], blue: [], channel: [] },
+				)
+			console.log('BARK!', nextState)
 			return {
 				...state,
 				isLoading: false,
-				flathead: newFish.flathead,
-				blue: newFish.blue,
-				channel: newFish.channel,
+				...nextState,
 			}
 		default:
 			return state
